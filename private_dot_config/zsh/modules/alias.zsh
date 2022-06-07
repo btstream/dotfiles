916b3b034f2a68c84c99fbde5040f54e432e0ac9
btstream <btstream@gmail.com>
2022-06-08 00:25:40 +0800

    modify youtube-dl parameters, to use aria2 for download

--- private_dot_config/zsh/modules/alias.zsh
+++ private_dot_config/zsh/modules/alias.zsh
@@ -1,2 +1,6 @@
 alias more="bat"
-alias youtube-dl="youtube-dl -f 'bestvideo[ext=mp4,height<=720]+bestaudio[ext=m4a]/bestvideo[ext=mp4,height<=480]+bestaudio[ext=m4a]/best[ext=mp4,height<=480]/best' --proxy socks5://127.0.0.1:1080"
+alias youtube-dl="youtube-dl -f \
+'bestvideo[ext=mp4,height<=720]+bestaudio[ext=m4a]/\
+bestvideo[ext=mp4,height<=480]+bestaudio[ext=m4a]/\
+best[ext=mp4,height<=480]/best' --proxy http://127.0.0.1:1081 \
+--external-downloader aria2c --external-downloader-args '-x 10'"
