ca6e7a2ed32fe6f6987ed249c51a932840a39be0
btstream <btstream@gmail.com>
2022-06-08 00:12:12 +0800

    add youtube-dl alias

--- private_dot_config/zsh/modules/alias.zsh
+++ private_dot_config/zsh/modules/alias.zsh
@@ -1,1 +1,2 @@
 alias more="bat"
+alias youtube-dl="youtube-dl -f 'bestvideo[ext=mp4,height<=720]+bestaudio[ext=m4a]/bestvideo[ext=mp4,height<=480]+bestaudio[ext=m4a]/best[ext=mp4,height<=480]/best' --proxy socks5://127.0.0.1:1080"
