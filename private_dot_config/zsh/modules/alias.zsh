3c358a6d10ace46afc1dc0aae1ae112929523cd2
btstream <btstream@gmail.com>
2022-06-09 10:46:12 +0800

    change zsh alias config

--- private_dot_config/zsh/modules/alias.zsh
+++ private_dot_config/zsh/modules/alias.zsh
@@ -1,6 +1,22 @@
-alias more="bat"
-alias youtube-dl="youtube-dl -f \
+## use bat to replace more
+if [[ "$(whence -p bat)" ]]; then
+    export BAT_THEME=OneHalfDark
+    alias more="bat"
+fi
+
+## youtube-dl scripts
+if [[ "$(whence -p youtube-dl)" ]]; then
+    alias youtube-dl="youtube-dl -f \
 'bestvideo[ext=mp4,height<=720]+bestaudio[ext=m4a]/\
 bestvideo[ext=mp4,height<=480]+bestaudio[ext=m4a]/\
 best[ext=mp4,height<=480]/best' --proxy http://127.0.0.1:1081 \
 --external-downloader aria2c --external-downloader-args '-x 10'"
+fi
+
+## use exa to replace ls
+if [[ "$(whence -p exa)" ]]; then
+    alias ls="exa --time-style iso"
+    alias ll="ls -lh"
+    alias la="ll -a"
+    alias lg="ll --git"
+fi
--- private_dot_config/zsh/modules/env.zsh
+++ private_dot_config/zsh/modules/env.zsh
@@ -1,7 +1,2 @@
 export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 export LANG=zh_CN.UTF-8
-
-#--------------------------------------------------------------------#
-#                            bat settings                            #
-#--------------------------------------------------------------------#
-export BAT_THEME=OneHalfDark
