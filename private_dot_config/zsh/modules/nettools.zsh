a1672316ab8f0d6c5d46c4d6e0a21f222414839b
btstream <btstream@gmail.com>
2022-08-17 10:41:39 +0800

    update nettools to support v2rayn windows

--- private_dot_config/zsh/modules/nettools.zsh
+++ private_dot_config/zsh/modules/nettools.zsh
@@ -2,15 +2,21 @@
 # tools for network
 ########################
 PROXY_HOST="127.0.0.1"
+SOCKS_PORT="1080"
+HTTP_PORT="1081"
+HTTP_PROXY_ALL_PORT="1083"
 if [[ $(uname -r | grep -i microsoft | wc -l) -eq 1 ]]; then
     PROXY_HOST=$(cat /etc/resolv.conf | grep nameserver | cut -d ' ' -f 2)
+    SOCKS_PORT="1082"
+    HTTP_PORT="1083"
+    HTTP_PROXY_ALL_PORT="1083"
 fi
 
 proxyset()
 {
 
     if [[ $1 == "git" ]]; then
-        git config --global http.proxy "socks5://$PROXY_HOST:1080"
+        git config --global http.proxy "socks5://$PROXY_HOST:$SOCKS_PORT"
         return
     fi
 
@@ -20,9 +26,9 @@ proxyset()
     fi
 
     if [[ $1 == "all" ]]; then
-        proxy="http://$PROXY_HOST:1083"
+        proxy="http://$PROXY_HOST:$HTTP_PROXY_ALL_PORT"
     else
-        proxy="http://$PROXY_HOST:1081"
+        proxy="http://$PROXY_HOST:$HTTP_PORT"
     fi
     export http_proxy=$proxy
     export https_proxy=$proxy
