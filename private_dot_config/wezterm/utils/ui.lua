3703a0e5e92ea54bf1157a7e55591e227293e3bc
btstream <btstream@gmail.com>
2023-08-23 19:23:06 +0800

    add application icon indicator framework for wezterm's tab panel

--- private_dot_config/wezterm/utils/ui.lua
+++ private_dot_config/wezterm/utils/ui.lua
@@ -3,22 +3,47 @@ local platform = require("utils").platform
 
 local app_icons = {
     nvim = wezterm.nerdfonts.dev_vim,
+    vim = wezterm.nerdfonts.dev_vim,
+
     ssh = wezterm.nerdfonts.md_ssh,
+    lazygit = wezterm.nerdfonts.dev_git,
+
+    btop = wezterm.nerdfonts.md_monitor_dashboard,
 }
 
 local function gen_app_icon(pane)
     local icon = nil
+
+    if pane.foreground_process_name == nil then
+        return nil
+    end
+
+    local sep = platform() == "Windows" and "\\" or "/"
+    local app = pane.foreground_process_name:split(sep)
+
+    app = app[#app]
+    if app then
+        if platform() == "Windows" then
+            app = app:split(".")[1]
+        end
+        print(app)
+        icon = app_icons[app]
+    end
+
     return icon
 end
 
 local function gen_tab_icon(pane)
     local domain_name = pane.domain_name
 
-    -- print(pane.foreground_process_name)
-
     -- set icon indicator
     local icon
     if domain_name == "local" then
+        icon = gen_app_icon(pane)
+        if icon then
+            return icon
+        end
+
         local p = platform()
         if p == "Windows" then
             icon = wezterm.nerdfonts.md_microsoft_windows
