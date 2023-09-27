56564594146a2a15b742cf00fdc4e6dd9418dac6
btstream <btstream@gmail.com>
2023-09-27 14:17:30 +0800

    change wezterm's terminfo updatee logic

--- private_dot_config/zsh/modules/shell-integration.zsh
+++ private_dot_config/zsh/modules/shell-integration.zsh
@@ -1,5 +1,8 @@
+if [[ ! -e ~/.terminfo/ ]]; then
+   mkdir ~/.terminfo
+fi
 if [[ $TERM_PROGRAM == "WezTerm" ]]; then
-    if [[ ! -f ~/.terminfo/77/wezterm ]]; then
+    if [[ $(find ~/.terminfo/ -name wezterm -type f | wc -l) == 0 ]]; then
         # mkdir -pv ~/.terminfo/
         tempfile=$(mktemp) \
             && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo \
