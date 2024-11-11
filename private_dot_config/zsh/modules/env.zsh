22379b9cba6fd1240f1ff67b363bf38ed70933fc
btstream <btstream@gmail.com>
2024-11-11 20:09:50 +0800

    change default sans-serif font to fira-sans and honor-sans

--- private_dot_config/zsh/modules/env.zsh
+++ private_dot_config/zsh/modules/env.zsh
@@ -4,11 +4,11 @@ export LANG=zh_CN.UTF-8
 export PATH="$HOME/.local/bin/:$PATH"
 if [[ $(whence -p fzf) ]]; then
     export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
-        --color=fg:#ffffff,fg+:#eeffff,bg:#212121,bg+:#4a4a4a
+        --color=fg:-1,fg+:#eeffff,bg:-1,bg+:#4a4a4a
         --color=hl:#82aaff,hl+:#89ddff,info:#ffcb6b,marker:#c3e88d
         --color=prompt:#82aaff,spinner:#c792ea,pointer:#8aaaff,header:#89ddff
         --color=border:#4a4a4a,label:#b2ccd6,query:#eeffff
         --border="double" --border-label="" --preview-window="border-rounded" --margin="1"
-        --prompt=" " --marker="" --pointer="█" --separator="─"
+        --prompt="  " --marker="" --pointer="█" --separator="─"
         --scrollbar="┃" --layout="reverse"'
 fi
