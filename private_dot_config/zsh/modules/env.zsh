233b0e30bb5cda4504d8edf51e8f5a62b7ecffba
btstream <btstream@gmail.com>
2024-11-29 20:39:58 +0800

    change some default settings

--- private_dot_config/zsh/modules/env.zsh
+++ private_dot_config/zsh/modules/env.zsh
@@ -7,8 +7,8 @@ if [[ $(whence -p fzf) ]]; then
         --color=fg:-1,fg+:#eeffff,bg:-1,bg+:#4a4a4a
         --color=hl:#82aaff,hl+:#89ddff,info:#ffcb6b,marker:#c3e88d
         --color=prompt:#82aaff,spinner:#c792ea,pointer:#8aaaff,header:#89ddff
-        --color=border:#4a4a4a,label:#b2ccd6,query:#eeffff
-        --border="double" --border-label="" --preview-window="border-rounded" --margin="1"
+        --color=border:#82aaff,label:#b2ccd6,query:#eeffff
+        --border="rounded" --border-label="" --preview-window="border-rounded" --margin="1"
         --prompt="  " --marker="" --pointer="█" --separator="─"
         --scrollbar="┃" --layout="reverse"'
 fi
